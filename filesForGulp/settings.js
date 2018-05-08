c1889b2ca0e72132affcb20c8c73e7690528fbd3
Anatolii <anatolii.lukavenko@gmail.com>
2018-05-08 18:09:32 +0300

    several changes for relationship of program and event, menu tab programs

---
+++ filesForGulp/settings.js
@@ -0,0 +1,2 @@
+exports.themeLocation = './wp-content/themes/University_theme/';
+exports.urlToPreview = 'http://lnauniversity-site/'; 
--- single-program.php
+++ single-program.php
@@ -20,44 +20,49 @@ while (have_posts()) {
       <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program'); ?>"><i class="fa fa-home" aria-hidden="true"></i> All programs </a> <span class="metabox__main"><?php the_title(); ?></span></p>
     </div>
 
-  	<div class="generic-content">
-  		<?php the_content(); ?>
-  	</div>
+    <div class="generic-content">
+      <?php the_content(); ?>
+    </div>
 
-			<?php
-          $today = date('Ymd');
-          $homepageEvents = new WP_Query(array(
-              'posts_per_page' => 2,
-              'post_type' => 'event', 
-              'meta_key' => 'event_date', 
-              'orderby' => 'meta_value_num',
-              'order' => 'ASC',
-              'meta_query' => array(
-                  array(
-                    'key' => 'event_date',
-                    'compare' => '>=',
-                    'value' => $today,
-                    'type' => 'numeric'
-                  ),
-                  array(
-                  	'key' => 'related_program',
-                    'compare' => 'LIKE',
-                    'value' => '"' . get_the_ID() . '"'
-                  )
-              )
-          ));
+    <?php
+    $today = date('Ymd');
+    $homepageEvents = new WP_Query(array(
+      'posts_per_page' => 2,
+      'post_type' => 'event', 
+      'meta_key' => 'event_date', 
+      'orderby' => 'meta_value_num',
+      'order' => 'ASC',
+      'meta_query' => array(
+        array(
+          'key' => 'event_date',
+          'compare' => '>=',
+          'value' => $today,
+          'type' => 'numeric'
+        ),
+        array(
+         'key' => 'related_program',
+         'compare' => 'LIKE',
+         'value' => '"' . get_the_ID() . '"'
+       )
+      )
+    ));
 
-          while ($homepageEvents->have_posts()) {
-            $homepageEvents->the_post();?>
-             <div class="event-summary">
+    if ($homepageEvents->have_posts()) {
+
+      echo '<hr class="section-break">';
+      echo '<h2 class="healine headline--medium">Upcoming ' . get_the_title() . ' Events</h2>';
+
+      while ($homepageEvents->have_posts()) {
+        $homepageEvents->the_post();?>
+        <div class="event-summary">
           <a class="event-summary__date t-center" href="#">
             <span class="event-summary__month"> <?php 
-              $eventDate = new DateTime(get_field('event_date'));
-              echo $eventDate->format('M'); 
-             ?></span>
+            $eventDate = new DateTime(get_field('event_date'));
+            echo $eventDate->format('M'); 
+            ?></span>
             <span class="event-summary__day"><?php 
-              echo $eventDate->format('d');
-             ?></span>  
+            echo $eventDate->format('d');
+            ?></span>  
           </a>
           <div class="event-summary__content">
             <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
@@ -66,20 +71,24 @@ while (have_posts()) {
             } else{
               echo wp_trim_words(get_the_content(), 4);
             }
-             ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+            ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
           </div>
         </div>
-         <?php }
-         ?>
+        <?php }
+        
+      }
 
 
-  </div>
+      ?>
 
 
-<?php  }
+    </div>
+
+
+    <?php  }
 
 
 
-get_footer();
+    get_footer();
 
- ?>
+    ?>
