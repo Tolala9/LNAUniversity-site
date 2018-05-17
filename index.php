b82f32b7f4d215760dd38443b32a85232dcc50ca
Anatolii <anatolii.lukavenko@gmail.com>
2018-05-17 11:53:29 +0300

    Wp: Use in all required files PageBanner function

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
 <?php 
  get_header(); ?> 
 
- <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg')?>);"></div>
-    <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title">Wellcome to our blog!</h1>
-      <div class="page-banner__intro">
-        <p>Keep up with our latest news</p>
-      </div>
-    </div>  
-  </div>
+ <?php pageBanner(array(
+  'title' => 'Wellcome to our blog!',
+  'subtitle' => 'Keep up with our latest news'
+ )); ?>
+
+ 
 
 index page
 <div class="container container--narrow page-section">
