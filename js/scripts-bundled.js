61d4756105104073a676c4e9624b002b581a4c1a
Anatolii <anatolii.lukavenko@gmail.com>
2018-05-22 17:37:11 +0300

    JS: made synchronous way to get JSON for posts and pages

--- js/scripts-bundled.js
+++ js/scripts-bundled.js
@@ -13664,11 +13664,15 @@ function () {
       var _this = this;
 
       _jquery.default.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val(), function (posts) {
-        _this.resultsDiv.html("\n\t\t\t\t\t<h2 class=\"search-overlay__section-title\">General Information</h2>\n\t\t\t\t\t".concat(posts.length ? '<ul class="link-list min-list">' : '<p>No general information matches that search.</p>', "\n\t\t\t\t\t\t").concat(posts.map(function (item) {
-          return "<li><a href=\"".concat(item.link, "\">").concat(item.title.rendered, "</a></li>");
-        }).join(''), "\n\t\t\t\t\t").concat(posts.length ? '</ul>' : '', "\n\t\t\t\t"));
+        _jquery.default.getJSON(universityData.root_url + '/wp-json/wp/v2/pages?search=' + _this.searchField.val(), function (pages) {
+          var combinedResults = posts.concat(pages);
 
-        _this.isSpinnerVisible = false;
+          _this.resultsDiv.html("\n\t\t\t\t\t<h2 class=\"search-overlay__section-title\">General Information</h2>\n\t\t\t\t\t".concat(combinedResults.length ? '<ul class="link-list min-list">' : '<p>No general information matches that search.</p>', "\n\t\t\t\t\t\t").concat(combinedResults.map(function (item) {
+            return "<li><a href=\"".concat(item.link, "\">").concat(item.title.rendered, "</a></li>");
+          }).join(''), "\n\t\t\t\t\t").concat(combinedResults.length ? '</ul>' : '', "\n\t\t\t\t"));
+
+          _this.isSpinnerVisible = false;
+        });
       });
     }
   }, {
