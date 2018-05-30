a898901a6d2e9b2223084b7b9d5741eeb3b148f5
Anatolii <anatolii.lukavenko@gmail.com>
2018-05-30 11:56:42 +0300

    WP: user roles and permissions

--- filesForPlugins/university-post-types.php
+++ filesForPlugins/university-post-types.php
@@ -2,6 +2,8 @@
 function university_post_types() {
 	//Event post type
  	register_post_type('event', array(
+    'capability_type' => 'event',
+    'map_meta_cap' => true,
  		'supports' => array('title', 'editor', 'excerpt'),
  		'rewrite' => array('slug' => 'events'),
  		'has_archive' => true,
@@ -17,7 +19,7 @@ function university_post_types() {
  	));
 //Program post type
  	register_post_type('program', array(
- 		'supports' => array('title', 'editor'),
+ 		'supports' => array('title'),
  		'rewrite' => array('slug' => 'programs'),
  		'has_archive' => true,
  	 'public' => true,
@@ -31,7 +33,41 @@ function university_post_types() {
  	 )
  	));
 
+ 	// Professor Post Type
+  register_post_type('professor', array(
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor', 'thumbnail'),
+    'public' => true,
+    'labels' => array(
+      'name' => 'Professors',
+      'add_new_item' => 'Add New Professor',
+      'edit_item' => 'Edit Professor',
+      'all_items' => 'All Professors',
+      'singular_name' => 'Professor'
+    ),
+    'menu_icon' => 'dashicons-welcome-learn-more'
+  ));
+
+  //Campus post type
+  register_post_type('campus', array(
+    'capability_type' => 'campus',
+    'map_meta_cap' => true,
+    'supports' => array('title', 'editor', 'excerpt'),
+    'rewrite' => array('slug' => 'campuses'),
+    'has_archive' => true,
+   'public' => true,
+   'menu_icon' => 'dashicons-location-alt',
+   'labels' => array(
+      'name' => 'Campuses',
+      'add_new_item' => 'Add New Campus',
+      'edit_item' => 'Edit Campus',
+      'all_items' => 'All Campuses',
+      'singular_name' => 'Campus'
+   )
+  ));
+
  }
+
  add_action('init', 'university_post_types');
  
 
