12b15a3a5faed639045ac3f54176140f9a3e7152
Anatolii <anatolii.lukavenko@gmail.com>
2018-05-31 15:31:45 +0300

    JS: Connect 'My Notes' with REST API [js method delete setting]

--- js/scripts-bundled.js
+++ js/scripts-bundled.js
@@ -13768,7 +13768,21 @@ function () {
   }, {
     key: "deleteNote",
     value: function deleteNote() {
-      alert("you clicked delete");
+      _jquery.default.ajax({
+        beforeSend: function beforeSend(xhr) {
+          xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
+        },
+        url: universityData.root_url + '/wp-json/wp/v2/note/100',
+        type: 'DELETE',
+        success: function success(response) {
+          console.log("Congrats");
+          console.log(response);
+        },
+        error: function error(response) {
+          console.log("Sory");
+          console.log(response);
+        }
+      });
     }
   }]);
 
