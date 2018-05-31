19e258f60d5aa0e0fce4342aec34490805b0276e
Anatolii <anatolii.lukavenko@gmail.com>
2018-05-31 16:05:30 +0300

    JS: Delete & Edit note methods completed

--- js/scripts-bundled.js
+++ js/scripts-bundled.js
@@ -13763,18 +13763,29 @@ function () {
     key: "events",
     value: function events() {
       (0, _jquery.default)(".delete-note").on("click", this.deleteNote);
+      (0, _jquery.default)(".edit-note").on("click", this.editNote);
     } // Methods will go here
 
   }, {
+    key: "editNote",
+    value: function editNote(e) {
+      var thisNote = (0, _jquery.default)(e.target).parents("li");
+      thisNote.find(".note-title-field, .note-body-field").removeAttr("readonly").addClass("note-active-field");
+      thisNote.find(".update-note").addClass("update-note--visible");
+    }
+  }, {
     key: "deleteNote",
-    value: function deleteNote() {
+    value: function deleteNote(e) {
+      var thisNote = (0, _jquery.default)(e.target).parents("li");
+
       _jquery.default.ajax({
         beforeSend: function beforeSend(xhr) {
           xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
         },
-        url: universityData.root_url + '/wp-json/wp/v2/note/100',
+        url: universityData.root_url + '/wp-json/wp/v2/note/' + thisNote.data('id'),
         type: 'DELETE',
         success: function success(response) {
+          thisNote.slideUp();
           console.log("Congrats");
           console.log(response);
         },
