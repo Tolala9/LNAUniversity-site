19104658f172dfa66f8d51312575916c69b5c92f
Anatolii <anatolii.lukavenko@gmail.com>
2018-05-31 18:05:48 +0300

    JS: Creating note feature completed

--- js/scripts-bundled.js
+++ js/scripts-bundled.js
@@ -13762,9 +13762,10 @@ function () {
   _createClass(MyNotes, [{
     key: "events",
     value: function events() {
-      (0, _jquery.default)(".delete-note").on("click", this.deleteNote);
-      (0, _jquery.default)(".edit-note").on("click", this.editNote.bind(this));
-      (0, _jquery.default)(".update-note").on("click", this.updateNote.bind(this));
+      (0, _jquery.default)("#my-notes").on("click", ".delete-note", this.deleteNote);
+      (0, _jquery.default)("#my-notes").on("click", ".edit-note", this.editNote.bind(this));
+      (0, _jquery.default)("#my-notes").on("click", ".update-note", this.updateNote.bind(this));
+      (0, _jquery.default)(".submit-note").on("click", this.createNote.bind(this));
     } // Methods will go here
 
   }, {
@@ -13837,6 +13838,34 @@ function () {
         success: function success(response) {
           _this.makeNoteReadOnly(thisNote);
 
+          console.log("Congrats");
+          console.log(response);
+        },
+        error: function error(response) {
+          console.log("Sory");
+          console.log(response);
+        }
+      });
+    }
+  }, {
+    key: "createNote",
+    value: function createNote(e) {
+      var ourNewPost = {
+        'title': (0, _jquery.default)(".new-note-title").val(),
+        'content': (0, _jquery.default)(".new-note-body").val(),
+        'status': 'publish'
+      };
+
+      _jquery.default.ajax({
+        beforeSend: function beforeSend(xhr) {
+          xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
+        },
+        url: universityData.root_url + '/wp-json/wp/v2/note/',
+        type: 'POST',
+        data: ourNewPost,
+        success: function success(response) {
+          (0, _jquery.default)(".new-note-title, .new-note-body").val('');
+          (0, _jquery.default)("\n\t\t\t\t\t<li data-id=\"".concat(response.id, "\">\n\t\t\t\t\t<input readonly class=\"note-title-field\" value=\"").concat(response.title.raw, "\" >\n\t\t\t\t\t<span class=\"edit-note\"> <i class=\"fa fa-pencil\" aria-hidden=\"true\"></i>Edit</span>\n\t\t\t\t\t<span class=\"delete-note\"> <i class=\"fa fa-trash-o\" aria-hidden=\"true\"></i>Delete</span>\n\t\t\t\t\t<textarea readonly class=\"note-body-field\">").concat(response.content.raw, "</textarea>\n\t\t\t\t\t<span class=\"update-note btn btn--blue btn--small\"> <i class=\"fa fa-arrow-right\" aria-hidden=\"true\"></i>Save</span>\n\t\t\t\t\t</li>\n\t\t\t\t\t")).prependTo("#my-notes").hide().slideDown();
           console.log("Congrats");
           console.log(response);
         },
